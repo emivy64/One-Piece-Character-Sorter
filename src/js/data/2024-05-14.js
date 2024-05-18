d87068fab5463d907b22bcfcc8a9faa83f52e8b6
emivy64 <153437313+emivy64@users.noreply.github.com>
2024-05-17 21:19:24 -0300

    Update 2024-05-14.js

--- src/js/data/2024-05-14.js
+++ src/js/data/2024-05-14.js
@@ -97,14 +97,14 @@ dataSet[dataSetVersion].options = [
       name: "Alvida",
       img: "011Alvida.jpeg",
       opts: {
-        saga: [ "s1", "s3", "s6", "s9", "s11"],
+        saga: [ "s1", "s3", "s6", "s10", "s11"],
       }
     },
     {
       name: "Koby",
       img: "012Koby.jpeg",
       opts: {
-        saga: [ "s1", "s4", "s6", "s9", "s11"],
+        saga: [ "s1", "s4", "s6", "s9", "s10", "s11"],
       }
     },
     {
@@ -125,7 +125,7 @@ dataSet[dataSetVersion].options = [
       name: "Helmeppo",
       img: "015Helmeppo.png",
       opts: {
-        saga: [ "s1", "s4", "s6", "s9", "s11"],
+        saga: [ "s1", "s4", "s6", "s9", "s10", "s11"],
       }
     },
     {
@@ -160,28 +160,28 @@ dataSet[dataSetVersion].options = [
       name: "Buggy",
       img: "020Buggy.jpeg",
       opts: {
-        saga: [ "s1", "s3", "s6", "s9", "s11"],
+        saga: [ "s1", "s3", "s6", "s10", "s11"],
       }
     },
     {
       name: "Cabaji",
       img: "021Cabaji.jpeg",
       opts: {
-        saga: [ "s1", "s3", "s6", "s9", "s11"],
+        saga: [ "s1", "s3", "s6", "s10", "s11"],
       }
     },
     {
       name: "Mohji",
       img: "022Mohji.jpeg",
       opts: {
-        saga: [ "s1", "s3", "s6", "s9", "s11"],
+        saga: [ "s1", "s3", "s6", "s10", "s11"],
       }
     },
     {
       name: "Richie",
       img: "023Richie.jpeg",
       opts: {
-        saga: [ "s1", "s3", "s6", "s9", "s11"],
+        saga: [ "s1", "s3", "s6", "s10", "s11"],
         ani: true
       }
     },
@@ -201,17 +201,290 @@ dataSet[dataSetVersion].options = [
       }
     },
     {
+      name: "Gaimon",
+      img: "027Gaimon.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
       name: "Usopp",
-      img: "028 - Usopp.jpeg",
+      img: "028Usopp.jpeg",
       opts: {
         saga: [ "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11"],
       }
     },
     {
+      name: "Piiman",
+      img: "029Piiman.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Ninjin",
+      img: "030Ninjin.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Tamanegi",
+      img: "031Tamanegi.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Kaya",
+      img: "032Kaya.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Kuro",
+      img: "033Kuro.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Merry",
+      img: "034Merry.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Jango",
+      img: "035Jango.jpeg",
+      opts: {
+        saga: [ "s1", "s2", "s6", "s9", "s11"],
+      }
+    },
+    {
+      name: "Sham",
+      img: "036Sham.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Buchi",
+      img: "037Buchi.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Banchina",
+      img: "038Banchina.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Yosaku",
+      img: "039Yosaku.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Johnny",
+      img: "040Johnny.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Fullbody",
+      img: "041Fullbody.jpeg",
+      opts: {
+        saga: [ "s1", "s2", "s6", "s9", "s11"],
+      }
+    },
+    {
       name: "Sanji",
-      img: "042 - Sanji.jpeg",
+      img: "042Sanji.jpeg",
       opts: {
         saga: [ "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11"],
+      }
+    },
+    {
+      name: "Zeff",
+      img: "043Zeff.jpeg",
+      opts: {
+        saga: [ "s1", "s9"],
+      }
+    },
+    {
+      name: "Patty",
+      img: "044Patty.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Gin",
+      img: "045Gin.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Carne",
+      img: "046Carne.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Don Krieg",
+      img: "047Krieg.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Dracule Mihawk",
+      img: "042Sanji.jpeg",
+      opts: {
+        saga: [ "s1", "s3", "s6", "s7", "s8", "s10", "s11"],
+      }
+    },
+    {
+      name: "Pearl",
+      img: "049Pearl.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Nezumi",
+      img: "050Nezumi.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Arlong",
+      img: "051Arlong.jpeg",
+      opts: {
+        saga: [ "s1", "s7"],
+      }
+    },
+    {
+      name: "Chew",
+      img: "052Chew.jpeg",
+      opts: {
+        saga: [ "s1", "s7"],
+      }
+    },
+    {
+      name: "Kuroobi",
+      img: "053Kuroobi.jpeg",
+      opts: {
+        saga: [ "s1", "s7"],
+      }
+    },
+    {
+      name: "Hatchan",
+      img: "054Hatchan.jpeg",
+      opts: {
+        saga: [ "s1", "s6", "s7"],
+      }
+    },
+    {
+      name: "Chabo",
+      img: "055Chabo.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Nojiko",
+      img: "056Nojiko.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Genzo",
+      img: "057Genzo.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Mohmoo",
+      img: "058Mohmoo.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Pudding Pudding",
+      img: "059PuddingPudding.png",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Bellemere",
+      img: "060Bellemere.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Stainless",
+      img: "061Stainless.jpeg",
+      opts: {
+        saga: [ "s1", "s3", "s6", "s10"],
+      }
+    },
+    {
+      name: "Brannew",
+      img: "062Brannew.jpeg",
+      opts: {
+        saga: [ "s1", "s6", "s8", "s10", "s11"],
+      }
+    },
+    {
+      name: "John Giant",
+      img: "063JohnGiant.jpeg",
+      opts: {
+        saga: [ "s1", "s6"],
+      }
+    },
+    {
+      name: "Tashigi",
+      img: "064Tashigi.jpeg",
+      opts: {
+        saga: [ "s1", "s2", "s4", "s6", "s8", "s11"],
+      }
+    },
+    {
+      name: "Ipponmatsu",
+      img: "065Ipponmatsu.jpeg",
+      opts: {
+        saga: [ "s1"],
+      }
+    },
+    {
+      name: "Smoker",
+      img: "066Smoker.jpeg",
+      opts: {
+        saga: [ "s1", "s2", "s4", "s6", "s8"],
+      }
+    },
+    {
+      name: "Dragon",
+      img: "067Dragon.jpeg",
+      opts: {
+        saga: [ "s1", "s4", "s6", "s9", "s10", "s11"],
       }
     },
     {
